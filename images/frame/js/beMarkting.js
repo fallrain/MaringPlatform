7f4f4009e38bcdd731d2c5105e5684de6a772532
王广开 <416266475@qq.com>
2016-12-06 14:54:49 +0800

    验证

--- images/frame/js/beMarkting.js
+++ images/frame/js/beMarkting.js
@@ -25,6 +25,7 @@ var BeMarking = Class.extend({
           },
           idCardNo: {
             required: true,
+            isIdCardNo: true,
           },
         },
         messages: {}
@@ -52,7 +53,7 @@ var BeMarking = Class.extend({
       $('.js-prg-' + i + 'more').show();
     }
   },
-  openMktAccnt: function(){
+  openMktAccnt: function(sucFn){
     /*打开弹出层*/
     this.selfObj.MktAccntDg = layer.open({
       type: 1,
@@ -63,9 +64,7 @@ var BeMarking = Class.extend({
       resize: false,
       area: ['940px', '490px'],
       content: $('#mp-updAccount-prg-main'),
-      end: function(){
-        //layer.tips('Hi', '#about', {tips: 1})
-      }
+      success: sucFn
     });
   },
   applyMarketAccount: function(e){
@@ -75,7 +74,7 @@ var BeMarking = Class.extend({
       Common.sendFormData(mpUrlsObj.brMkt.applyMarketAccount,
         function(data){
           if(data.isSuccess){
-
+            _this.choosePrg(3);
           }else{
             alert(data.resultMsg);
           }
@@ -90,60 +89,69 @@ var BeMarking = Class.extend({
   },
   queryUser: function(isOpened){
     var _this = this;
-    if(!isOpened){
-      _this.openMktAccnt();
-    }
     Common.sendFormData(mpUrlsObj.brMkt.queryUser,
       function(data){
         if(data.isSuccess){
-          var loginNameDOM = $('#' + _this.DOMId.loginName);
-          loginNameDOM.html(data.data.loginName);
-          var accountType = data.type;
-          var updAccountBtn = $('#' + _this.DOMId.updAccountBtn);
-          var updAccountBtnFn = function(type){
-            updAccountBtn.html('完善账户信息成为营销账户');
-            switch(type){
-              case 1:
-                window.open(mpUrlsObj.brMkt.bindMobile);
-                break;
-              case 2:
-                window.open(mpUrlsObj.brMkt.bindEmail);
-                break;
-              case 3:
-                beMarking.choosePrg(2);
+          var userStatus = data.data.status;
+          if(userStatus == 1 || userStatus == 2){
+            //1是初始状态
+            if(!isOpened){
+              _this.openMktAccnt(function(){
+                if(userStatus == 1){
+                  var loginNameDOM = $('#' + _this.DOMId.loginName);
+                  loginNameDOM.html(data.data.loginName);
+                  var accountType = data.type;
+                  var updAccountBtn = $('#' + _this.DOMId.updAccountBtn);
+                  var updAccountBtnFn = function(type){
+                    updAccountBtn.html('完善账户信息成为营销账户');
+                    switch(type){
+                      case 1:
+                        window.open(mpUrlsObj.brMkt.bindMobile);
+                        break;
+                      case 2:
+                        window.open(mpUrlsObj.brMkt.bindEmail);
+                        break;
+                      case 3:
+                        _this.choosePrg(2);
+                    }
+                    //事件执行完替换事件，再次请求，判断是否完善过账户
+                    updAccountBtn[0].onclick = function(){
+                      //true代表不需要再次弹窗
+                      _this.queryUser(true);
+                    };
+                  };
+                  if(!data.data.mobile){
+                    updAccountBtn.html('去绑定手机号码');
+                    layer.tips('手机号没有绑定', '#' + _this.DOMId.updAccountBtn, {
+                      tips: [1, '#de143a'],
+                      time: 3000
+                    });
+                    updAccountBtn[0].onclick = function(){
+                      updAccountBtnFn(1);
+                    };
+                  }else if(!data.data.email){
+                    updAccountBtn.html('去绑定邮箱');
+                    layer.tips('邮箱没有绑定', '#' + _this.DOMId.updAccountBtn, {
+                      tips: [1, '#de143a'],
+                      time: 3000
+                    });
+                    updAccountBtn[0].onclick = function(){
+                      updAccountBtnFn(2);
+                    };
+                  }else{
+                    updAccountBtn.html('完善账户信息成为营销账户');
+                    updAccountBtn[0].onclick = function(){
+                      updAccountBtnFn(3);
+                    };
+                  }
+                }else{
+                  _this.choosePrg(3);
+                }
+              });
             }
-            //事件执行完替换事件，再次请求，判断是否完善过账户
-            updAccountBtn[0].onclick = function(){
-              //true代表不需要再次弹窗
-              _this.queryUser(true);
-            };
-          };
-          if(!data.mobile){
-            updAccountBtn.html('去绑定手机号码');
-            layer.tips('手机号没有绑定', '#' + _this.DOMId.updAccountBtn, {
-              tips: [1, '#de143a'],
-              time: 3000
-            });
-            updAccountBtn[0].onclick = function(){
-              updAccountBtnFn(1);
-            };
-          }else if(!data.email){
-            updAccountBtn.html('去绑定邮箱');
-            layer.tips('邮箱没有绑定', '#' + _this.DOMId.updAccountBtn, {
-              tips: [1, '#de143a'],
-              time: 3000
-            });
-            updAccountBtn[0].onclick = function(){
-              updAccountBtnFn(2);
-            };
-          }else{
-            updAccountBtn[0].onclick = function(){
-              updAccountBtnFn(3);
-            };
           }
         }else{
-          //错误关闭对话框
-          layer.close(_this.selfObj.MktAccntDg);
+          //layer.close(_this.selfObj.MktAccntDg);
           var loginUrl = data.data.loginUrl;
           var returnUrl = window.location.href;
           location.href = loginUrl + "?returnUrl=" + returnUrl;
